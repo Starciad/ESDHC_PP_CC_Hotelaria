6d785e123a240c513a2ba1846f786799f88bae76
juuliahelena <juliah300106@gmail.com>
2026-04-28 09:39:36 -0300

    Add flow modifications.

--- src/Hotelaria.Server/Models/Flow.cs
+++ src/Hotelaria.Server/Models/Flow.cs
@@ -2,6 +2,9 @@ namespace Hotelaria.Server
 {
     public sealed class Flow
     {
+        public int Id { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
         
     }
 }
