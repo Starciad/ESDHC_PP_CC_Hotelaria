22d97a6d4d24311360ab7465bb2b57ca5f9e4a8b
Starciad <davigamerlsf16@gmail.com>
2026-05-28 15:48:21 -0300

    feat: incluir estrutura HTML e estilos CSS.

---
+++ src/Hotel.Web/Controllers/ExploreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.Web.Controllers
+{
+    public class ExploreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
