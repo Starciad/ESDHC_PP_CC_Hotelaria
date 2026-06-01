6ba3bbb897b85d5364f1475e8cb9bd228e55fad9
Starciad <davigamerlsf16@gmail.com>
2026-06-01 16:36:10 -0300

    remove: tabelas não utilizadas do banco.

---
+++ src/Hotel.Web/Migrations/20260601193529_RemoveUnnecessaryTables.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUnnecessaryTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Flows");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AdmissionDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CPF = table.Column<string>(type: "TEXT", maxLength: 14, nullable: false),
+                    IsAdministrator = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    PIS = table.Column<string>(type: "TEXT", maxLength: 14, nullable: true),
+                    Salary = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployeeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    GuestId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReserveId = table.Column<int>(type: "INTEGER", nullable: false),
+                    RoomId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CheckInDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CheckOutDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Flows_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flows_Guests_GuestId",
+                        column: x => x.GuestId,
+                        principalTable: "Guests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flows_Reserves_ReserveId",
+                        column: x => x.ReserveId,
+                        principalTable: "Reserves",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Flows_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FlowId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Flows_FlowId",
+                        column: x => x.FlowId,
+                        principalTable: "Flows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flows_EmployeeId",
+                table: "Flows",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flows_GuestId",
+                table: "Flows",
+                column: "GuestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flows_ReserveId",
+                table: "Flows",
+                column: "ReserveId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flows_RoomId",
+                table: "Flows",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_FlowId",
+                table: "Payments",
+                column: "FlowId");
+        }
+    }
+}
